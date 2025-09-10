3b1b0862d2a24aab8f75ce2888bac042a1902780
Basil Crow <me@basilcrow.com>
2025-09-10 08:10:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Jersey 3 API Plugin
+
+This plugin provides the [JAX-RS 3.1](https://jakarta.ee/specifications/restful-ws/3.1/) and [Jersey 3](https://eclipse-ee4j.github.io/jersey/) APIs for other plugins.
---
+++ src/test/java/org/glassfish/jersey/JerseyTest.java
@@ -0,0 +1,32 @@
+package org.glassfish.jersey;
+
+import static org.junit.Assert.assertEquals;
+
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import java.net.URI;
+import org.glassfish.jersey.client.ClientConfig;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.RealJenkinsRule;
+
+public class JerseyTest {
+
+    @Rule
+    public RealJenkinsRule rr = new RealJenkinsRule();
+
+    @Test
+    public void json() throws Throwable {
+        rr.then(JerseyTest::_json);
+    }
+
+    private static void _json(JenkinsRule j) throws Exception {
+        j.jenkins.getActions().add(new JerseyResource());
+        ClientConfig clientConfig = new ClientConfig();
+        Client client = ClientBuilder.newClient(clientConfig);
+        URI uri = new URI(j.getURL() + "jersey/employee?q=json");
+        Employee employee = client.target(uri).request().get(Employee.class);
+        assertEquals("Noah", employee.getFirstName());
+    }
+}
